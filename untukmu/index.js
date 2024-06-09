50062368a768880a3cbb7ed5586ae4ab6bc10b09
jep2710 <anwarunid@gmail.com>
2024-06-09 14:15:08 +0700

    apalah

--- untukmu/index.js
+++ untukmu/index.js
@@ -1,34 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = document.getElementById('audio');
+    const playPauseBtn = document.getElementById('play-pause');
 
-const searchSong =() =>{
-    let songName = document.getElementById('search-field').value
-    fetch(`https://api.lyrics.ovh/suggest/${songName}`)
-    .then(res => res.json())
-    .then(data => displaySong(data.data))
-}
-
-const displaySong = songs => {
-    const songContainer = document.getElementById("song-container")
-    songContainer.innerHTML = ''
-    songs.forEach(song => {
-        console.log(song)
-        console.log(song.album.cover_medium)
-        const songDiv = document.createElement("div")
-        songDiv.className = "single-result row align-items-center my-3 p-3"
-        songDiv.innerHTML = `
-        
-                    <div class="col-lg-2 text-center">
-                        <img class src=${song.album.cover_medium} style="height:50px ;" alt=""/>
-                    </div>
-                    <div class="col-lg-4 text-center">
-                        <h3 class="lyrics-name">${song.title}</h3>
-                        <p class="author lead">Album by <span>${song.artist.name}</span></p>
-                    </div>
-                    <div class="col-lg-6 text-center">   
-                        <audio controls>
-                            <source src=${song.preview} type="">
-                        </audio>
-                    </div>
-                            `
-        songContainer.appendChild(songDiv)
+    playPauseBtn.addEventListener('click', function() {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
     });
-}
+});
+
